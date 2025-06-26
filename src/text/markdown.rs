4ca4aef9138e5410b895b15443c78b10b53eeb72
carbonflora <zihaoliang0413@gmail.com>
2025-06-25 21:58:16 -0700

    revert

--- src/text/markdown.rs
+++ src/text/markdown.rs
@@ -2,19 +2,15 @@ use markdown::{mdast::Node, ParseOptions};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct RKBMarkdown {
-    node: Node,
+    root: Node,
 }
 
-impl RKBMarkdown {
-    pub fn to_string(&self) -> String {
-        self.node.to_string()
-    }
-}
+impl RKBMarkdown {}
 
 impl From<String> for RKBMarkdown {
     fn from(value: String) -> Self {
         let options = ParseOptions::gfm();
         let node = markdown::to_mdast(&value, &options).unwrap();
-        Self { node }
+        Self { root: node }
     }
 }
